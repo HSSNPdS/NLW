ae886f730ede14a38c41ff82d050567a41d00c62
Natan Prado <hssnpds@gmail.com>
2020-06-07 22:19:41 -0300

    Primeiro commit

---
+++ public/scripts/intro.js
@@ -0,0 +1,54 @@
+// comments
+// document.white("hello")
+
+// variaveis, tipos de dados
+// let myvar = "He"
+// const myconst = "He"
+
+// document.write(myconst + myvar)
+
+// string
+// "Isso é uma string"
+// 'Isso também é um string'
+// `Isso é uma string também`
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// objetos possuem propriedades e funcionalidades
+//const pessoa = {
+//    altura: "1,80m",
+//    idade: 24,
+//    solteiro: true,
+//    correr(){
+//        document.write("Correndo")
+//    }
+//}
+
+// pessoa.correr()
+
+// Array - Vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funções - Funcionalidades - Atalhos - Reuso de código
+
+// registrar função
+// function sayMyName(name){
+//    document.write(name)
+//}
+
+// executar
+// sayMyName("Douglas")
+// sayMyName("Jozé")
+// sayMyName("Maria")
+// sayMyName("Roberto")
+// sayMyName("Matheus")
